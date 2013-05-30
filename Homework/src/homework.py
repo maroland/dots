6707d992cc4e4bcae20a32b119262aed9da473d9
Emma <Emma@192.168.2.102>
2013-05-30 22:04:15 +0200

    test2

--- Homework/src/homework.py
+++ Homework/src/homework.py
@@ -1,9 +1,5 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-__author__="Emma"
-__date__ ="$30.05.2013 22:01:17$"
-
+#test
+def main():
+  print("hello again")
 if __name__ == "__main__":
-    print "Hello World"
+  main()
